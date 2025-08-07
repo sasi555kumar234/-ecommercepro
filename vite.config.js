8869cae86678e378bf1780dec328bd0d9153649b
sasikumar23 <2300032451@kluniversity.in>
2025-08-07 12:36:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/ecommerce/', // 👈 this is what you're asking about
   plugins: [react()],
 })
